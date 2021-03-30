32dd64bb4620ffe493e7ce6a5e5b3169c464a720
tenon <wanghexiang@flowpp.com>
2021-03-30 10:48:04 +0800

    Feat: 配置接口请求代理

--- .f2econfig.dev.js
+++ .f2econfig.dev.js
@@ -4,36 +4,43 @@
  * @type {import('f2e-server').F2EConfig}
  */
 const config = {
-    port: 8000, // 启动端口
-    livereload: true,   // 自动刷新
-    build: false,
-    gzip: true,
-    useLess: true,  // 使用less编译
-    buildFilter: pathname => /^(asserts|css|favicon|index|src|$)/.test(pathname),
-    middlewares: [
-        // 对应文本支持简单ejs语法的服务端渲染，通常用来给资源链接加时间戳
-        { middleware: 'template', test: /\.html?/ },
-        // 编译构建前端资源可以使用 esbuild 或者 webpack, esbuild更快、webpack兼容性更好
-        { middleware: 'esbuild' },
-        // 使用webpack 需要安装中间件 f2e-middle-webpack
-        // { middleware: 'webpack' },
-        
-        // {
-        //     middleware: 'proxy',
-        //     test: /^\/?meta/,
-        //     url: 'http://172.16.0.201:5001',
-        //     pathname: '/get',
-        // },
+  port: 8000,
+  livereload: true,
+  build: false,
+  gzip: true,
+  useLess: true,
+  buildFilter: pathname => /^(asserts|css|favicon|index|src|$)/.test(pathname),
+  middlewares: [
+    // 对应文本支持简单ejs语法的服务端渲染，通常用来给资源链接加时间戳
+    { middleware: 'template', test: /\.html?/ },
+    // 编译构建前端资源可以使用 esbuild 或者 webpack, esbuild更快、webpack兼容性更好
+    { middleware: 'esbuild' },
+    // 使用webpack 需要安装中间件 f2e-middle-webpack
+    // { middleware: 'webpack' },
 
-        require('./lib').default,
-        {middleware: 'authority'},
-    ],
-    // onServerCreate: (server) => {
-    //     const { Server } = require('ws')
-    //     const wss = new Server({server});
-    //     wss.on('connection', (socket) => {
-    //         socket.send('init')
-    //     })
-    // }
+    // {
+    //   middleware: 'proxy',
+    //   test: /^\/?meta/,
+    //   url: 'http://172.16.0.201:5001',
+    //   pathname: '/get',
+    // },
+
+    // rule-manage
+    {
+      middleware: 'proxy',
+      test: /^\/v1\/gpm/,
+      url: 'http://172.16.0.163:18080',
+    },
+
+    require('./lib').default,
+    { middleware: 'authority' },
+  ],
+  // onServerCreate: (server) => {
+  //     const { Server } = require('ws')
+  //     const wss = new Server({server});
+  //     wss.on('connection', (socket) => {
+  //         socket.send('init')
+  //     })
+  // }
 }
 module.exports = config
--- src/apis/meta.ts
+++ src/apis/meta.ts
@@ -4,3 +4,7 @@ import { Fetch } from "./Fetch"
 export const getRaw = (type: 'domain' | 'app' | 'action') => Fetch(`/meta?action=${type}_json`)
 
 export const meta_all = (): Promise<Meta> => Fetch('/api/meta')
+
+// export const doLogin = (data) => Fetch('/v1/gpm/user/login', data)
+
+// export const getOrgInfo = () => Fetch('/v1/gpm/org/infos?a=1&b=2')
